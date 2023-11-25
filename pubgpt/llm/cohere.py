c9e5ac766f3884620752bec49109984b97a6b771
Danilo Palumbo <48620457+dSupertramp@users.noreply.github.com>
2023-11-25 16:59:12 +0100

    Add get_associations to Cohere

--- pubgpt/llm/cohere.py
+++ pubgpt/llm/cohere.py
@@ -2,13 +2,14 @@ from langchain.embeddings import CohereEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.llms import Cohere
-from typing import List, Any
+from typing import List, Any, Tuple
 from dotenv import load_dotenv
+from .pre_prompt import pre_prompt
 
 load_dotenv()
 
 
-def create_embeddings(splitted_text: List) -> Any:
+def create_embeddings(splitted_text: List) -> CohereEmbeddings:
     """
     Create embeddings from chunks for Cohere.
 
@@ -44,3 +45,32 @@ def retriver(query: str, embeddings: Any) -> str:
         return_source_documents=True,
     )
     return result(query)["result"]
+
+
+def get_associations(pairs: List[Tuple[str, str]], embeddings: CohereEmbeddings) -> str:
+    pre_prompt_pairs: list = []
+    for index, item in enumerate(pairs, 1):
+        pre_prompt_pairs.append(
+            f"{index}) {item[0][0].strip()} associated with {item[1][0].strip()}?"
+        )
+    pre_prompt_pairs = "\n".join(pre_prompt_pairs)
+    query = f"""
+According to the text provided, can you tell me if:
+{pre_prompt_pairs.strip()}\n
+As result, provide me a CSV with:
+- Boolean result (only 'Yes' or 'No')
+- The entire part before the sentence "is associated with"
+- The entire part after the sentence "is associated with"
+For instance:
+'Yes,X,Y'
+Also, remove the numbers list (like 1)) from the CSV.
+    """.strip()
+    prompt = pre_prompt.format(query=query)
+    retriever = embeddings.as_retriever(search_type="similarity")
+    result = RetrievalQA.from_chain_type(
+        llm=Cohere(),
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+    )
+    return result(prompt)["result"]
